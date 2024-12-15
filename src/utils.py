a49a410fc34f099c41d1c918b29b566c6901acbe
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 23:18:14 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,6 @@
 import torch
 from torchvision import datasets
 
-def load_data():
-    mnist_train = datasets.MNIST("./data", train=True, download=True)
-    mnist_test = datasets.MNIST("./data", train=False, download=True)
-
-    x_train = mnist_train.data.reshape(-1, 784) / 255
-    y_train = mnist_train.targets
-
-    x_test = mnist_test.data.reshape(-1, 784) / 255
-    y_test = mnist_test.targets
-
-    return x_train, y_train, x_test, y_test
-
 def get_accuracy(scores: torch.Tensor, labels: torch.Tensor) -> int | float:
     predictions = torch.max(scores, 1).indices
     output = torch.eq(predictions, labels)
