bf725e4d116051e0817733cb47809559735d1f67
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 20:12:37 +0800

    Create train.py

---
+++ src/cnn/train.py
@@ -0,0 +1,28 @@
+%%time
+import torch
+import torch.nn as nn
+
+def train_model(loader: torch.utils.data.DataLoader, model: nn.Module):
+    optimiser = torch.optim.Adam(model.parameters())
+    loss_fn = nn.CrossEntropyLoss()
+    
+    epoch_losses = []
+    model = model.to(device)
+    for i in range(10):
+        epoch_loss = 0
+        model.train()
+        for idx, data in enumerate(loader):
+            x, y = data
+            x, y = x.to(device), y.to(device)
+            optimiser.zero_grad()
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            loss.backward()
+            optimiser.step()
+            epoch_loss = epoch_loss + loss.item()
+
+        epoch_loss = epoch_loss / len(loader)
+        epoch_losses.append(epoch_loss)
+        print ("Epoch: {}, Loss: {}".format(i, epoch_loss))
+
+    return model, epoch_losses
