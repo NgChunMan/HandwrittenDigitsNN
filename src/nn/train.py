6733d7ba9be9bd968d2c9f00d3eec8d8a9ed46e3
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 10:17:37 +0800

    Create train.py

---
+++ src/nn/train.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from src.DigitNet import DigitNet
+
+def train_model(x_train: torch.Tensor, y_train: torch.Tensor, epochs=20):
+    model = DigitNet(784, 10)
+    optimizer = torch.optim.Adam(model.parameters())
+    loss_fn = nn.CrossEntropyLoss()
+
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+
+        # Forward pass
+        y_pred = model(x_train)
+
+        # Compute loss
+        loss = loss_fn(y_pred, y_train)
+
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+
+    return model
