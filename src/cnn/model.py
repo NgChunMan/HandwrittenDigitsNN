3275909747bb0a166818801dcb0ea23227885394
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 20:04:40 +0800

    Create model.py

---
+++ src/cnn/model.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 14 * 14, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)  # Flatten
+        x = self.relu(self.fc1(x))
+        x = self.softmax(self.fc2(x))
+        return x
