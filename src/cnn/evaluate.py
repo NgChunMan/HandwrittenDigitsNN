06f3e05ce588d81769853894054039f458ffc8a4
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 20:49:41 +0800

    Create evaluate.py

---
+++ src/cnn/evaluate.py
@@ -0,0 +1,18 @@
+import torch
+from src.utils import get_accuracy
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def evaluate(model, test_loader):
+    """
+    Evaluate model accuracy on the test dataset.
+    """
+    model.eval()
+    
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            x, y = data
+            x, y = x.to(device), y.to(device)
+            pred_model = model(x)
+            accuracy = get_accuracy(pred_model, y)
+            print(f"drop-out (0.5) accuracy: {accuracy}")
