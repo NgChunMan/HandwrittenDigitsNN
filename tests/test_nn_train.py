4776d5fa0b8b78d7b2e4bd032a09cfa4e484788e
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 23:53:23 +0800

    Update test_nn_train.py

--- tests/test_nn_train.py
+++ tests/test_nn_train.py
@@ -1,8 +1,8 @@
 import torch
 from src.nn.model import DigitNet
-from src.nn.train import train_model
+from src.nn.train import train_nn_model
 
 def test_nn_train():
     x_train_new = torch.rand(5, 784, requires_grad=True)
     y_train_new = ones = torch.ones(5, dtype=torch.uint8)
-    assert type(train_model(x_train_new, y_train_new)) == DigitNet
+    assert type(train_nn_model(x_train_new, y_train_new)) == DigitNet
