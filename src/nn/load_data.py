7b273c8cdd5b0406a79b52e85a7419ceebbf9cbc
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 22:25:29 +0800

    Create load_data.py

---
+++ src/nn/load_data.py
@@ -0,0 +1,14 @@
+from torchvision import datasets
+
+def load_data():
+    # This downloads the MNIST datasets ~63MB
+    mnist_train = datasets.MNIST("./data", train=True, download=True)
+    mnist_test  = datasets.MNIST("./data", train=False, download=True)
+    
+    x_train = mnist_train.data.reshape(-1, 784) / 255
+    y_train = mnist_train.targets
+    
+    x_test = mnist_test.data.reshape(-1, 784) / 255
+    y_test = mnist_test.targets
+
+    return x_train, y_train, x_test, y_test
